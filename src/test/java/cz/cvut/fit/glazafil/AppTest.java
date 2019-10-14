2eff78b2f2fd9280ba00e7fa570e8e68d0f69fe8
Filip Glazar <glazafil@fit.cvut.cz>
2019-10-14 13:06:44 +0200

    [Add] Unit tests

---
+++ src/test/java/cz/cvut/fit/glazafil/AppTest.java
@@ -0,0 +1,47 @@
+package cz.cvut.fit.glazafil;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class AppTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Autowired
+    private Logger logger;
+
+
+    @Test
+    public void logTest() {
+        logger.log("Error", Level.ERROR);
+        assertEquals("[E-mail][ERROR]: Error\n", outContent.toString());
+    }
+
+}
